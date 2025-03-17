cccbaa595bce0f9b169b1e8df349ce6bfe41034a
Marta Olivé <martaolivep@gmail.com>
2025-03-17 12:09:03 +0100

    Esquema del stafilter posat

---
+++ temporal/STAFilter_example.js
@@ -0,0 +1,34 @@
+STAFilter={
+    "entity": "Datastreams",
+    "filterSchema": {
+        "0_0": {
+            "items": [
+                0,
+                1
+            ],
+            "nexus": " or "
+        }
+    },
+    "filterData": [
+        [
+            0,
+            "Datastreams",
+            [
+                "name"
+            ],
+            " = ",
+            "Air Temperature",
+            "text"
+        ],
+        [
+            1,
+            "Datastreams",
+            [
+                "name"
+            ],
+            " = ",
+            "Noise Level",
+            "text"
+        ]
+    ]
+}
